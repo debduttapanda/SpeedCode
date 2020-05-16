be5b12e6f56440f1c3651d1fdcf0a83d4ab58ba4
debdutta <debdutta@v-xplore.com>
2020-05-16 18:34:59 +0530

    Loger tag made customizable

--- speedcode/src/main/java/com/coderusk/speedcode/app/Loger.java
+++ speedcode/src/main/java/com/coderusk/speedcode/app/Loger.java
@@ -3,17 +3,27 @@ package com.coderusk.speedcode.app;
 import android.util.Log;
 
 public class Loger {
+    public static String TAG = "LOGER";
+
+    public static String getTAG() {
+        return TAG;
+    }
+
+    public static void setTAG(String TAG) {
+        Loger.TAG = TAG;
+    }
+
     public static void log(String msg) {
         try {
             int level = 3;
             StackTraceElement[] traces;
             traces = Thread.currentThread().getStackTrace();
             String display = (msg+" near "  + traces[level] + " " );
-            Log.d("BIKRIMART_PARTNER_LOGER",display);
+            Log.d(TAG,display);
         } catch (Exception e) {
             e.printStackTrace();
             String display = msg +" near Undetected location";
-            Log.d("BIKRIMART_PARTNER_LOGER",display);
+            Log.d(TAG,display);
         }
     }
 }
