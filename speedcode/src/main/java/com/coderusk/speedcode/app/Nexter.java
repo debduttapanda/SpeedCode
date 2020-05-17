362ac77dfa78a3a2242ebcd86bd8089cc716849e
debdutta <debdutta@v-xplore.com>
2020-05-17 11:39:00 +0530

    Nexter class added

---
+++ speedcode/src/main/java/com/coderusk/speedcode/app/Nexter.java
@@ -0,0 +1,57 @@
+package com.coderusk.speedcode.app;
+
+import java.util.ArrayList;
+
+public class Nexter
+    {
+        public interface Action
+        {
+            void act(Nexter nexter);
+        }
+
+        private int pos = -1;
+        private ArrayList<Action> actions;
+
+        private Nexter() {
+            this.actions = new ArrayList<>();
+        }
+
+        public static Nexter create()
+        {
+            return new Nexter();
+        }
+
+        public Nexter push(Action action)
+        {
+            actions.add(action);
+            return this;
+        }
+
+        public void execute()
+        {
+            Nexter.nexec(this);
+        }
+
+        public Action next()
+        {
+            ++pos;
+            if(pos>actions.size()-1)
+            {
+                return null;
+            }
+            return actions.get(pos);
+        }
+
+        public static void nexec(Nexter nexter)
+        {
+            if(nexter!=null)
+            {
+                Action listener = nexter.next();
+                if(listener!=null)
+                {
+                    listener.act(nexter);
+                }
+            }
+        }
+
+    }
